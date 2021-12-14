ed6faca4e9959b1d1eaad9b26a3db4c53a28a463
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-14 22:37:59 +0300

    Initialize android project

---
+++ app/src/main/java/by/alexandr7035/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.alexandr7035.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
