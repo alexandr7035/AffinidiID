3b71a8fb6301190b34978e5d093ae31b83eac650
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-14 23:37:01 +0300

    Initial styling

--- app/src/main/java/by/alexandr7035/affinidi_id/MainActivity.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/MainActivity.kt
@@ -1,11 +1,19 @@
 package by.alexandr7035.affinidi_id
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.navigationBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.TRANSPARENT
+        window.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.background_window))
     }
 }
