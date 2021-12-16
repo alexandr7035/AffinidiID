7bf5d0540c35fbcad62f815d538918a35a43ba37
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-16 15:57:29 +0300

    Handle errors in repository

--- app/src/main/java/by/alexandr7035/affinidi_id/data/ApiService.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/data/ApiService.kt
@@ -2,10 +2,11 @@ package by.alexandr7035.affinidi_id.data
 
 import by.alexandr7035.affinidi_id.data.model.SignInRequest
 import by.alexandr7035.affinidi_id.data.model.SignInResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("api/v1/users/login")
-    suspend fun signIn(@Body body: SignInRequest): SignInResponse
+    suspend fun signIn(@Body body: SignInRequest): Response<SignInResponse>
 }
--- app/src/main/java/by/alexandr7035/affinidi_id/data/implementation/AuthRepositoryImpl.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/data/implementation/AuthRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package by.alexandr7035.affinidi_id.data.implementation
 
+import by.alexandr7035.affinidi_id.core.AppError
 import by.alexandr7035.affinidi_id.core.ErrorType
 import by.alexandr7035.affinidi_id.data.ApiService
 import by.alexandr7035.affinidi_id.data.AuthRepository
 import by.alexandr7035.affinidi_id.data.SignUpModel
 import by.alexandr7035.affinidi_id.data.model.SignInRequest
 import by.alexandr7035.affinidi_id.data.model.SignInModel
+import by.alexandr7035.affinidi_id.data.model.SignInResponse
+import java.lang.Exception
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(private val apiService: ApiService): AuthRepository {
@@ -16,13 +19,35 @@ class AuthRepositoryImpl @Inject constructor(private val apiService: ApiService)
     }
 
     override suspend fun signIn(userName: String, password: String): SignInModel {
-//        return SignInResponse.Fail(ErrorType.USER_DOES_NOT_EXIST)
-//        return try {
-        val res = apiService.signIn(SignInRequest(userName, password))
-        return SignInModel.Success(res.accessToken, res.userDid)
-//        } catch (e: Exception) {
-//            // FIXME map exceptions to errortypes
-//            SignInResponse.Fail(ErrorType.UNKNOWN_ERROR)
-//        }
+
+        try {
+            val res = apiService.signIn(SignInRequest(userName, password))
+            if (res.isSuccessful) {
+                val data = res.body() as SignInResponse
+                return SignInModel.Success(data.accessToken, data.userDid)
+            }
+            else {
+                return when (res.code()) {
+                    400 -> {
+                        SignInModel.Fail(ErrorType.WRONG_USERNAME_OR_PASSWORD)
+                    }
+                    404 -> {
+                        SignInModel.Fail(ErrorType.USER_DOES_NOT_EXIST)
+                    }
+                    // Unknown fail code
+                    else -> {
+                        SignInModel.Fail(ErrorType.UNKNOWN_ERROR)
+                    }
+                }
+            }
+        }
+        // Handled in ErrorInterceptor
+        catch (appError: AppError) {
+           return SignInModel.Fail(appError.errorType)
+        }
+        // Unknown exception
+        catch (e: Exception) {
+            return SignInModel.Fail(ErrorType.UNKNOWN_ERROR)
+        }
     }
 }
