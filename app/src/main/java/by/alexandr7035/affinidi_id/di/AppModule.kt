8d1d1a090e2eb7950368928659f80fd7ca682b64
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-16 11:13:42 +0300

    Add AuthInterceptor.kt

--- app/src/main/java/by/alexandr7035/affinidi_id/di/AppModule.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/di/AppModule.kt
@@ -1,20 +1,55 @@
 package by.alexandr7035.affinidi_id.di
 
+import by.alexandr7035.affinidi_id.core.AuthInterceptor
+import by.alexandr7035.affinidi_id.data.ApiService
 import by.alexandr7035.affinidi_id.data.AuthRepository
 import by.alexandr7035.affinidi_id.data.implementation.AuthRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // FIXME move to network module
     @Provides
     @Singleton
-    fun provieAuthRepository(): AuthRepository {
-        return AuthRepositoryImpl()
+    fun provideHttpClient(): OkHttpClient {
+        return  OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .retryOnConnectionFailure(false)
+            .build()
+    }
+
+    // FIXME move to network module
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://cloud-wallet-api.prod.affinity-project.org/")
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provieAuthRepository(apiService: ApiService): AuthRepository {
+        return AuthRepositoryImpl(apiService)
     }
 }
