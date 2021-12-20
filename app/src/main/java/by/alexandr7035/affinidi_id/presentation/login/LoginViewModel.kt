8a5e6ec1165ce6be4aa41fb39fdfc30007a73b46
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-21 01:24:53 +0300

    Use SingleLiveEvent in order not to trigger events when navigate back

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginViewModel.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package by.alexandr7035.affinidi_id.presentation.login
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import by.alexandr7035.affinidi_id.core.livedata.SingleLiveEvent
 import by.alexandr7035.affinidi_id.data.AuthRepository
 import by.alexandr7035.affinidi_id.data.model.sign_in.SignInModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,7 +14,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val repository: AuthRepository): ViewModel() {
-    val signInLiveData = MutableLiveData<SignInModel>()
+    val signInLiveData = SingleLiveEvent<SignInModel>()
 
     fun signIn(userName: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/registration/RegistrationViewModel.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/registration/RegistrationViewModel.kt
@@ -3,6 +3,7 @@ package by.alexandr7035.affinidi_id.presentation.registration
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import by.alexandr7035.affinidi_id.core.livedata.SingleLiveEvent
 import by.alexandr7035.affinidi_id.data.AuthRepository
 import by.alexandr7035.affinidi_id.data.model.sign_up.SignUpConfirmationModel
 import by.alexandr7035.affinidi_id.data.model.sign_up.SignUpModel
@@ -15,8 +16,8 @@ import javax.inject.Inject
 @HiltViewModel
 class RegistrationViewModel @Inject constructor(private val repository: AuthRepository): ViewModel() {
 
-    val signUpLiveData = MutableLiveData<SignUpModel>()
-    val signUpConfirmationLiveData = MutableLiveData<SignUpConfirmationModel>()
+    val signUpLiveData = SingleLiveEvent<SignUpModel>()
+    val signUpConfirmationLiveData = SingleLiveEvent<SignUpConfirmationModel>()
 
     fun signUp(userName: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
