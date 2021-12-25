8fef2721fe22a8a40cc51ee3c79af819ec84f462
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-25 16:35:19 +0300

    Remove old input validators

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/helpers/InputValidationResult.kt
+++
@@ -1,7 +0,0 @@
-package by.alexandr7035.affinidi_id.presentation.helpers
-
-enum class InputValidationResult {
-    PASSWORD_IS_EMPTY,
-    PASSWORD_WRONG_FORMAT,
-    NO_ERRORS
-}
--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/helpers/InputValidator.kt
+++
@@ -1,5 +0,0 @@
-package by.alexandr7035.affinidi_id.presentation.helpers
-
-interface InputValidator {
-    fun validatePassword(password: String): InputValidationResult
-}
