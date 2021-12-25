57e8fb8ad1c1e08aa82c731a75895587a1c1a668
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-25 17:28:01 +0300

    Input validation on UI layer

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginViewModel.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import by.alexandr7035.affinidi_id.core.livedata.SingleLiveEvent
 import by.alexandr7035.affinidi_id.data.AuthRepository
+import by.alexandr7035.affinidi_id.data.helpers.validation.InputValidationHelper
+import by.alexandr7035.affinidi_id.data.helpers.validation.InputValidationResult
 import by.alexandr7035.affinidi_id.data.model.sign_in.SignInModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -13,7 +15,10 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val repository: AuthRepository): ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val repository: AuthRepository,
+    private val inputValidationHelper: InputValidationHelper
+) : ViewModel() {
     val signInLiveData = SingleLiveEvent<SignInModel>()
 
     fun signIn(userName: String, password: String) {
@@ -28,5 +33,13 @@ class LoginViewModel @Inject constructor(private val repository: AuthRepository)
 
     fun saveUserName(userName: String) {
         repository.saveUserName(userName)
+    }
+
+    fun validateUserName(userName: String): InputValidationResult {
+        return inputValidationHelper.validateUserName(userName)
+    }
+
+    fun validatePassword(password: String): InputValidationResult {
+        return inputValidationHelper.validatePassword(password)
     }
 }
