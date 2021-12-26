44955e5eb301c42fbe69cf5e1fb4a8e6d57cb41e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-26 14:17:05 +0300

    Regex for password validation

--- app/src/main/java/by/alexandr7035/affinidi_id/data/helpers/validation/InputValidationHelperImpl.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/data/helpers/validation/InputValidationHelperImpl.kt
@@ -3,35 +3,29 @@ package by.alexandr7035.affinidi_id.data.helpers.validation
 import android.util.Patterns
 
 class InputValidationHelperImpl: InputValidationHelper {
+
+    private val minPasswordLength = 8
+    private val passwordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{$minPasswordLength,}\$".toPattern()
+
     override fun validateUserName(userName: String): InputValidationResult {
         return when {
             userName.isBlank() -> InputValidationResult.EMPTY_FIELD
-
             ! Patterns.EMAIL_ADDRESS.matcher(userName).matches() -> InputValidationResult.WRONG_FORMAT
-
             else -> InputValidationResult.NO_ERRORS
         }
     }
 
     override fun validatePassword(password: String): InputValidationResult {
-
         return when {
-            password.isBlank() -> {
-                InputValidationResult.EMPTY_FIELD
-            }
-
-            // TODO validate here
-
+            password.isBlank() -> InputValidationResult.EMPTY_FIELD
+            ! passwordPattern.matcher(password).matches() -> InputValidationResult.WRONG_FORMAT
             else -> InputValidationResult.NO_ERRORS
         }
     }
 
     override fun validateConfirmationCode(confirmationCode: String): InputValidationResult {
         return when {
-            confirmationCode.isBlank() -> {
-                InputValidationResult.EMPTY_FIELD
-            }
-
+            confirmationCode.isBlank() -> InputValidationResult.EMPTY_FIELD
             else -> InputValidationResult.NO_ERRORS
         }
     }
