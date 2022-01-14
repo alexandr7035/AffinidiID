f9908366c8b82ac395b1548598f4c56508315319
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-15 00:03:54 +0300

    Remove models for change username
    #33

--- app/src/main/java/by/alexandr7035/affinidi_id/data/model/change_username/ChangeUserNameModel.kt
+++
@@ -1,9 +0,0 @@
-package by.alexandr7035.affinidi_id.data.model.change_username
-
-import by.alexandr7035.affinidi_id.core.ErrorType
-
-abstract class ChangeUserNameModel {
-    class Success(val newUserName: String): ChangeUserNameModel()
-
-    class Fail(val errorType: ErrorType): ChangeUserNameModel()
-}
