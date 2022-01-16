bb33da72458ded7670b33988a981d01b25fa9ebd
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-17 00:22:55 +0300

    Add extension for Long for string dates

---
+++ domain/src/main/java/by/alexandr7035/affinidi_id/domain/core/extensions/Long.kt
@@ -0,0 +1,19 @@
+package by.alexandr7035.affinidi_id.domain.core.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+// FIXME dry
+fun Long.getStringDateFromLong(
+    stringFormat: String,
+    timezoneStr: String? = null,
+    locale: Locale = Locale.US
+): String {
+    val format = SimpleDateFormat(stringFormat, locale)
+
+    if (timezoneStr != null) {
+        format.timeZone = TimeZone.getTimeZone(timezoneStr)
+    }
+
+    return format.format(this)
+}
