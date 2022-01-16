a3a24d05f530085dd4cee498d55c0f29910602da
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-16 17:40:24 +0300

    Add ResourceProvider

---
+++ app/src/main/java/by/alexandr7035/affinidi_id/di/PresentationModule.kt
@@ -0,0 +1,20 @@
+package by.alexandr7035.affinidi_id.di
+
+import android.content.Context
+import by.alexandr7035.affinidi_id.presentation.helpers.resources.ResourceProvider
+import by.alexandr7035.affinidi_id.presentation.helpers.resources.ResourceProviderImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object PresentationModule {
+
+    @Provides
+    fun provideResourceProvider(@ApplicationContext context: Context): ResourceProvider {
+        return ResourceProviderImpl(context)
+    }
+}
