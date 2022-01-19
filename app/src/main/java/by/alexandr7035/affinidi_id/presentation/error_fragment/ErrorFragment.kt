197af71bdd52d6a71bc2a09d040fd2f72823907e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-20 00:49:20 +0300

    Update UI for error fragment

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/error_fragment/ErrorFragment.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/error_fragment/ErrorFragment.kt
@@ -1,33 +1,38 @@
-package by.alexandr7035.affinidi_id.presentation.error_dialog
+package by.alexandr7035.affinidi_id.presentation.error_fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.alexandr7035.affinidi_id.R
-import by.alexandr7035.affinidi_id.databinding.FragmentErrorDialogBinding
+import by.alexandr7035.affinidi_id.databinding.FragmentErrorBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
-class ErrorDialogFragment : BottomSheetDialogFragment() {
-    private val args by navArgs<ErrorDialogFragmentArgs>()
-    private val binding by viewBinding(FragmentErrorDialogBinding::bind)
+class ErrorFragment : Fragment() {
+    private val args by navArgs<ErrorFragmentArgs>()
+    private val binding by viewBinding(FragmentErrorBinding::bind)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_error_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_error, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
         binding.errorTitle.text = args.title
         binding.errorMessage.text = args.message
 
         binding.closeBtn.setOnClickListener {
-            dialog?.dismiss()
+            findNavController().navigateUp()
         }
     }
 }
