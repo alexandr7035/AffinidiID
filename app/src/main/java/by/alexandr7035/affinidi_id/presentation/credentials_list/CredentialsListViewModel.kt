ef0fbec7fcd6a24dd5d54bcff7e59ea03dfe0d93
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-21 00:17:41 +0300

    Remove resourceProvider from viewmodel
    #41

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/credentials_list/CredentialsListViewModel.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/credentials_list/CredentialsListViewModel.kt
@@ -4,15 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import by.alexandr7035.affinidi_id.R
-import by.alexandr7035.affinidi_id.core.extensions.getStringDateFromLong
-import by.alexandr7035.affinidi_id.domain.model.credentials.common.VcType
-import by.alexandr7035.affinidi_id.domain.model.credentials.common.credential_subject.EmailCredentialSubject
-import by.alexandr7035.affinidi_id.domain.model.credentials.stored_credentials.CredentialStatus
-import by.alexandr7035.affinidi_id.domain.model.credentials.stored_credentials.CredentialsListResModel
 import by.alexandr7035.affinidi_id.domain.usecase.credentials.GetCredentialsListUseCase
-import by.alexandr7035.affinidi_id.presentation.credentials_list.vc_fields_recycler.VCFieldItem
-import by.alexandr7035.affinidi_id.presentation.helpers.resources.ResourceProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
@@ -23,7 +15,6 @@ import javax.inject.Inject
 @HiltViewModel
 class CredentialsListViewModel @Inject constructor(
     private val getCredentialsListUseCase: GetCredentialsListUseCase,
-    private val resourceProvider: ResourceProvider,
     private val mapper: CredentialsListMapper
 ) : ViewModel() {
 
