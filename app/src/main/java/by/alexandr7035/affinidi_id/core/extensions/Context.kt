0f16748a6504047f579379cc038c8b6793d18fd9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-24 00:52:09 +0300

    Add extension to vibrate

--- app/src/main/java/by/alexandr7035/affinidi_id/core/extensions/Context.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/core/extensions/Context.kt
@@ -1,8 +1,27 @@
 package by.alexandr7035.affinidi_id.core.extensions
 
 import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
 
 fun Context.showToast(message: String, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(this, message, duration).show()
+}
+
+
+fun Context.vibrate(vibrationTimeMills: Long) {
+    val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        this.getSystemService(Context.VIBRATOR_MANAGER_SERVICE)
+    } else {
+        this.getSystemService(Context.VIBRATOR_SERVICE)
+    } as Vibrator
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        vibrator.vibrate(VibrationEffect.createOneShot(vibrationTimeMills, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+    else {
+        vibrator.vibrate(vibrationTimeMills)
+    }
 }
