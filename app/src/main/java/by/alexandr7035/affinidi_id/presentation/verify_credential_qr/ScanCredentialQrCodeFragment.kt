9f7df8e6669a4ec618a497b7c0b964f3c1198564
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-02-07 23:35:25 +0300

    Remove unused code

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/verify_credential_qr/ScanCredentialQrCodeFragment.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/verify_credential_qr/ScanCredentialQrCodeFragment.kt
@@ -1,26 +1,15 @@
 package by.alexandr7035.affinidi_id.presentation.verify_credential_qr
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import by.alexandr7035.affinidi_id.R
-import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
-
-import com.journeyapps.barcodescanner.ScanContract
-
-import com.journeyapps.barcodescanner.ScanOptions
-
-import androidx.activity.result.ActivityResultLauncher
 import by.alexandr7035.affinidi_id.core.extensions.debug
 import by.alexandr7035.affinidi_id.core.extensions.showToast
-import by.alexandr7035.affinidi_id.core.extensions.vibrate
 import by.alexandr7035.affinidi_id.databinding.FragmentScanCredentialQrCodeBinding
-import by.alexandr7035.affinidi_id.presentation.common.VibrationMode
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.journeyapps.barcodescanner.ScanIntentResult
 import timber.log.Timber
 
 
@@ -37,17 +26,6 @@ class ScanCredentialQrCodeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.qrCodeView.setStatusText("")
-
-        // Register the launcher and result handler
-        val barcodeLauncher = registerForActivityResult(
-            ScanContract()
-        ) { result: ScanIntentResult ->
-            if (result.contents == null) {
-                Toast.makeText(requireActivity(), "Cancelled", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(requireActivity(), "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-            }
-        }
 
         binding.qrCodeView.decodeSingle { scanResult ->
             Timber.debug("barcode result $scanResult")
