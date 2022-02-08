f1ba25dd233efeb46118c647cf77a432b75b92f4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-02-09 01:13:03 +0300

    Ui logic for camera permissions
    #90

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/verify_credential_qr/ScanCredentialQrCodeFragment.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/verify_credential_qr/ScanCredentialQrCodeFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.alexandr7035.affinidi_id.R
 import by.alexandr7035.affinidi_id.core.extensions.debug
 import by.alexandr7035.affinidi_id.core.extensions.navigateSafe
-import by.alexandr7035.affinidi_id.core.extensions.showToast
+import by.alexandr7035.affinidi_id.core.extensions.openAppSystemSettings
 import by.alexandr7035.affinidi_id.core.extensions.vibrate
 import by.alexandr7035.affinidi_id.databinding.FragmentScanCredentialQrCodeBinding
 import by.alexandr7035.affinidi_id.presentation.common.VibrationMode
@@ -35,23 +35,36 @@ class ScanCredentialQrCodeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        // Show QR scanner by default
+        binding.permissionsErrorLayout.isVisible = false
+        binding.qrCodeLayout.isVisible = true
+
         // Ask for camera permissions
         when (viewModel.askForCameraPermissions(requireActivity())) {
-            PermissionsCheckResult.PERMISSION_GRANTED -> {}
+            PermissionsCheckResult.PERMISSION_GRANTED -> {
+                // Start QR code scanning
+                binding.qrCodeView.resume()
+            }
 
             PermissionsCheckResult.SHOULD_REQUEST_PERMISSION -> {
                 viewModel.requestCameraPermissions(requireActivity())
             }
 
             PermissionsCheckResult.SHOULD_REDIRECT_TO_SETTINGS -> {
-                // TODO dialog
-                requireContext().showToast("Should redirect to settings")
+                // Show error view
+                binding.permissionsErrorLayout.isVisible = true
+                binding.qrCodeLayout.isVisible = false
             }
         }
 
-
+        // Disable text label on qrcode view
         binding.qrCodeView.setStatusText("")
 
+        // On QR scanned
         binding.qrCodeView.decodeSingle { scanResult ->
             Timber.debug("barcode result $scanResult")
             binding.qrCodeView.pause()
@@ -61,6 +74,10 @@ class ScanCredentialQrCodeFragment : Fragment() {
                 scanResult.text
             ))
         }
+
+        binding.settingsButton.setOnClickListener {
+            requireContext().openAppSystemSettings()
+        }
     }
 
     override fun onPause() {
@@ -68,9 +85,31 @@ class ScanCredentialQrCodeFragment : Fragment() {
         binding.qrCodeView.pause()
     }
 
+
+    // Chack permissions again when return from app system settings page
     override fun onResume() {
         super.onResume()
-        binding.qrCodeView.resume()
+
+        // Ask for camera permissions
+        when (viewModel.askForCameraPermissions(requireActivity())) {
+            PermissionsCheckResult.PERMISSION_GRANTED -> {
+                binding.permissionsErrorLayout.isVisible = false
+                binding.qrCodeLayout.isVisible = true
+                // Start QR code scanning
+                binding.qrCodeView.resume()
+            }
+
+            PermissionsCheckResult.SHOULD_REQUEST_PERMISSION -> {
+               // Don't ask in onResume as it's called every time
+               // When permissions dialog dismisses
+            }
+
+            PermissionsCheckResult.SHOULD_REDIRECT_TO_SETTINGS -> {
+                // Show error view
+                binding.permissionsErrorLayout.isVisible = true
+                binding.qrCodeLayout.isVisible = false
+            }
+        }
     }
 
 }
