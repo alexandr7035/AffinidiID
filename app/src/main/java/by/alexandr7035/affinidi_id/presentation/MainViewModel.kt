f6318e5d1f89a874d0b8c54853636441390b6d1e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-02-10 01:52:31 +0300

    Save intermediate result

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/MainViewModel.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import by.alexandr7035.affinidi_id.core.livedata.SingleLiveEvent
+import by.alexandr7035.affinidi_id.domain.core.ErrorType
 import by.alexandr7035.affinidi_id.domain.model.auth_check.AuthCheckResModel
 import by.alexandr7035.affinidi_id.domain.usecase.user.AuthCheckUseCase
 import by.alexandr7035.affinidi_id.domain.usecase.user.GetAuthStateUseCase
@@ -25,12 +26,14 @@ class MainViewModel @Inject constructor(
         return getAuthStateUseCase.execute().isAuthorized
     }
 
-    fun statAuthCheck() {
+    fun startAuthCheck() {
         viewModelScope.launch(Dispatchers.IO) {
             val res = authCheckUseCase.execute()
 
             withContext(Dispatchers.Main) {
-                authCheckLiveData.value = res
+//                authCheckLiveData.value = res
+                // FIXME debug
+                authCheckLiveData.value = AuthCheckResModel.Fail(ErrorType.AUTHORIZATION_ERROR)
             }
         }
     }
