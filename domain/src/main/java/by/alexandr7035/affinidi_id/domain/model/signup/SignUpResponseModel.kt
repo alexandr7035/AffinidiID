51a1c7c49b533a7fd293bda54468475e4a6e9b28
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-03-15 01:07:46 +0300

    Add username to SignUpResponseModel.kt
    43980b703f23301ac

--- domain/src/main/java/by/alexandr7035/affinidi_id/domain/model/signup/SignUpResponseModel.kt
+++ domain/src/main/java/by/alexandr7035/affinidi_id/domain/model/signup/SignUpResponseModel.kt
@@ -4,7 +4,8 @@ import by.alexandr7035.affinidi_id.domain.core.ErrorType
 
 sealed class SignUpResponseModel {
     data class Success(
-        val confirmSignUpToken: String
+        val confirmSignUpToken: String,
+        val userName: String
     ): SignUpResponseModel()
 
     data class Fail(
