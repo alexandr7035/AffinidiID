e109a1aa953597e4f086ae9d1117dd4bf3a28d7e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-07-04 01:29:38 +0300

    Add copyright note

--- app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginFragment.kt
+++ app/src/main/java/by/alexandr7035/affinidi_id/presentation/login/LoginFragment.kt
@@ -1,6 +1,8 @@
 package by.alexandr7035.affinidi_id.presentation.login
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
@@ -25,15 +27,10 @@ import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
 @AndroidEntryPoint
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private val viewModel by viewModels<LoginViewModel>()
     private val binding by viewBinding(FragmentLoginBinding::bind)
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -83,10 +80,12 @@ class LoginFragment : Fragment() {
                         ErrorType.USER_DOES_NOT_EXIST -> {
                             binding.userNameField.error = getString(R.string.error_user_not_found)
                         }
+
                         ErrorType.WRONG_USERNAME_OR_PASSWORD -> {
                             binding.userNameField.error = getString(R.string.error_wrong_user_or_password)
                             binding.passwordField.error = getString(R.string.error_wrong_user_or_password)
                         }
+
                         ErrorType.FAILED_CONNECTION -> {
                             binding.root.showSnackBar(
                                 message = getString(R.string.error_failed_connection),
@@ -107,27 +106,12 @@ class LoginFragment : Fragment() {
             }
         }
 
-
-        val goToSignUpText = getString(R.string.go_to_sign_up)
-        val spannable = goToSignUpText.getClickableSpannable(
-            clickableText = getString(R.string.go_to_sign_up_clickable),
-            clickListener = {
-                findNavController().navigateSafe(LoginFragmentDirections.actionLoginFragmentToSignUpGraph())
-            },
-            isBold = true,
-            spannableColor = ContextCompat.getColor(requireContext(), R.color.gray_500)
-        )
-
-        binding.goToSignUpBtn.apply {
-            text = spannable
-            movementMethod = LinkMovementMethod.getInstance()
-            highlightColor = Color.TRANSPARENT
-        }
-
         binding.forgotPasswordBtn.setOnClickListener {
             findNavController()
                 .navigateSafe(LoginFragmentDirections.actionLoginFragmentToResetPasswordGraph())
         }
+
+        setupSpannable()
     }
 
     private fun checkIfFormIsValid(): Boolean {
@@ -165,5 +149,41 @@ class LoginFragment : Fragment() {
         }
 
         return formIsValid
+    }
+
+    private fun setupSpannable() {
+        val goToSignUpText = getString(R.string.go_to_sign_up)
+        val signUpSpan = goToSignUpText.getClickableSpannable(
+            clickableText = getString(R.string.go_to_sign_up_clickable),
+            clickListener = {
+                findNavController().navigateSafe(LoginFragmentDirections.actionLoginFragmentToSignUpGraph())
+            },
+            isBold = true,
+            spannableColor = ContextCompat.getColor(requireContext(), R.color.gray_500)
+        )
+
+        binding.goToSignUpBtn.apply {
+            text = signUpSpan
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.TRANSPARENT
+        }
+
+        val copyrightText = getString(R.string.copyright_note)
+        val copyrightSpan = copyrightText.getClickableSpannable(
+            clickableText = getString(R.string.copyright_span),
+            clickListener = {
+                val url = getString(R.string.copyright_site)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            },
+            isBold = true,
+            spannableColor = ContextCompat.getColor(requireContext(), R.color.gray_500)
+        )
+
+        binding.copyrightBtn.apply {
+            text = copyrightSpan
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.TRANSPARENT
+        }
     }
 }
